48ea9288546b29c11238cf661b2d509b80e26e1e
Taimoor Ali <taimoorali314@gmail.com>
2022-12-21 16:30:55 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain
+          src="./assets/logo.jpeg" transition="scale-transition" width="100" />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        <span class="mr-2">URL Shortner</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="text-center">
+
+    <h1>Short URL </h1>
+    <p>ShortURL is a url shortener to reduce a long link. Use our tool to shorten links and then share them, in addition
+      you can monitor traffic statistics.</p>
+    <br />
+    <br />
+
+    <div>
+      <v-text-field label="Enter Link Here" v-model="url"></v-text-field>
+    </div>
+
+    <v-btn color="primary" elevation="2" large @click="convertToShort">Convert</v-btn>
+
+    <div>
+      <v-text-field label="Shorted URL" v-model="resulturl"></v-text-field>
+    </div>
+
+    <div class="mt-9">
+      <h2>Want More? Try Premium Features!</h2>
+      <p>Custom short links, powerful dashboard, detailed analytics, API, UTM builder, QR codes,<br/> browser extension, 50+
+        app integrations and support. Only $14/month.
+      </p>
+      <v-btn color="primary" elevation="2" large >Go Premium</v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: () => ({
+
+    url: '',
+    resulturl: ''
+
+  }),
+  methods: {
+
+    convertToShort() {
+      const url = new URL(
+        "https://t.ly/api/v1/link/shorten"
+      );
+
+      const params = {
+        "api_token": "2n0aMBboj2ecAE9k3OO8kOiQzHIB4FG3TVUdNSU2JjSwXtq8H8lluYmGhocu",
+      };
+      Object.keys(params)
+        .forEach(key => url.searchParams.append(key, params[key]));
+
+      const headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      };
+
+      let body = {
+        "long_url": this.url
+      };
+
+      fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+      }).then(response => response.json()).then(data => {
+        console.log(data.short_url)
+        this.resulturl = data.short_url;
+      })
+
+    }
+
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
